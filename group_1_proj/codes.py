a4ccfe1f3d366d70c66ca26bbbfad1a51bc404ce
Dan Andreescu <d.andreescu@student.tudelft.nl>
2021-01-13 14:50:10 +0100

    merge plot_fidelity -> Jord

--- group_1_proj/codes.py
+++ group_1_proj/codes.py
@@ -41,13 +41,17 @@ def get_random_noise_matrix():
 
 def uniform_uncorrelated_noise(p_error):
     def apply_noise(circuit):
+        any_corrupted = False
         for i in range(circuit.num_qubits):
             if random.random() < p_error:
-                print("qubit ", i, " corrupted")
+                print("qubit", i, "corrupted")
+                any_corrupted = True
                 matrix = get_random_noise_matrix()
                 err_op = Operator(matrix)
-                print(matrix)
                 circuit.unitary(err_op, [i], label='error')
+
+        if not any_corrupted:
+            print("no qubits corrupted")
     return apply_noise
 
 
@@ -72,18 +76,22 @@ def bit_flip(apply_noise):
     circuit.cx(q[0], q[2])
     circuit.ccx(q[2], q[1], q[0])
 
-    # SIMULATION
     qi_job = execute(circuit, backend=qi_backend, shots=1)
     qi_result = qi_job.result()
-    histogram = qi_result.get_counts(circuit)
+    probabilities_histogram = qi_result.get_probabilities(circuit)
 
-    final_state = 2
-    for state, counts in histogram.items():
-        print("The initial state was 0, and after the process it is ", state[2])
-        final_state = int(state[2])
+    correct_prob = 0.0
+    wrong_prob = 0.0
+    for state, val in probabilities_histogram.items():
+        if int(state[2]) == 0:
+            correct_prob += val
+        else:
+            wrong_prob += val
 
-    assert final_state != 2
-    return final_state
+    print("The initial state was 0, and after the process it is 0 with probability", round(correct_prob, 3))
+    print("The initial state was 0, and after the process it is 1 with probability", round(wrong_prob, 3))
+
+    return correct_prob
 
 
 def shor(apply_noise):
@@ -131,55 +139,62 @@ def shor(apply_noise):
     circuit.cx(q[0], q[6])
     circuit.toffoli(q[6], q[3], q[0])
 
-    # SIMULATION
-    qi_job = execute(circuit, backend=qi_backend, shots=256)
+    # SIMULATION (SHOTS MUST BE EQUAL TO 1 HERE!!!) (otherwise the optimization is not triggered correctly)
+    qi_job = execute(circuit, backend=qi_backend, shots=1)
     qi_result = qi_job.result()
-    histogram = qi_result.get_counts(circuit)
+    probabilities_histogram = qi_result.get_probabilities(circuit)
 
-    final_state = 2
-    final_states = []
-    for state, counts in histogram.items():
-        print("The initial state was 0, and after the process it is ", state[8])
-        final_state = int(state[8])
-        final_states.append(state[8])
+    correct_prob = 0.0
+    wrong_prob = 0.0
+    for state, val in probabilities_histogram.items():
+        if int(state[8]) == 0:
+            correct_prob += val
+        else:
+            wrong_prob += val
 
-    assert final_state != 2
-    return final_state
+    print("The initial state was 0, and after the process it is 0 with probability", round(correct_prob, 3))
+    print("The initial state was 0, and after the process it is 1 with probability", round(wrong_prob, 3))
+
+    return correct_prob
 
 
 def la_flamme(apply_noise):
     QI.set_authentication(get_token_authentication(load_account()), QI_URL)
     qi_backend = QI.get_backend('QX single-node simulator')
 
-    q = QuantumRegister(5, "q")
-    c = ClassicalRegister(1, "c")  # Only 1 bit for measuring the state of the qubit
-
+    nbits = 5
+    q = QuantumRegister(nbits, "q")
+    c = ClassicalRegister(nbits, "c")
     qc = QuantumCircuit(q, c, name="Laflamme logical qubit")
 
     qc.h(0)
     qc.h(1)
     qc.h(3)
 
-    multi = QuantumCircuit(1,
-                           name='pi phase')  # Creates a new cicuit in which the effect of the multi control/target gate is enclosed
+    # Creates a new cicuit in which the effect of the multi control/target gate is enclosed
+    multi = QuantumCircuit(1, name='pi phase')
     multi.z(0)
-    multi_gate = multi.to_gate()  # Makes a gate out of the circuit
+    # Generate the actual gate
+    multi_gate = multi.to_gate()
 
-    cmulti_z_gate0 = multi_gate.control(3)  # Makes it a controlled gate, with three controls
-    cmulti_z_gate1 = multi_gate.control(3,
-                                        ctrl_state='010')  # Other gate, but also specifies in which state the control qubits should be in
+    # Makes it a controlled gate, with three controls
+    cmulti_z_gate0 = multi_gate.control(3)
+    # Other gate, but also specifies in which state the control qubits should be in
+    cmulti_z_gate1 = multi_gate.control(3, ctrl_state='010')
     cmulti_z_gate2 = multi_gate.control(2)
 
-    qc.append(cmulti_z_gate0, [1, 2, 3, 4])  # Adds the gates to the existing circuit with the [ control , target ]
+    # Adds the gates to the existing circuit with the [control, target]
+    qc.append(cmulti_z_gate0, [1, 2, 3, 4])
     qc.append(cmulti_z_gate1, [1, 2, 3, 4])
 
     qc.cx(2, 4)
 
-    multi = QuantumCircuit(2, name='c-x')  # Same as before, but this time two qubits are affected by gate
-    multi.x(0)
-    multi.x(1)
-    multi_gate = multi.to_gate()
-    cmulti_x_gate = multi_gate.control()
+    # Same as before, but this time two qubits are affected by gate
+    multi2 = QuantumCircuit(2, name='c-x')
+    multi2.x(0)
+    multi2.x(1)
+    multi_gate2 = multi2.to_gate()
+    cmulti_x_gate = multi_gate2.control()
 
     qc.append(cmulti_x_gate, [0, 2, 4])
 
@@ -189,47 +204,56 @@ def la_flamme(apply_noise):
 
     qc.append(cmulti_z_gate2, [4, 3, 2])
 
-    ########################################## Errors can occur here #######################################################
-
+    #################################################################################################
     # ERROR PHASE
+    # todo: implement error here
     apply_noise(qc)
+    #################################################################################################
 
-    qc = qc + qc.inverse()  # Decoder is the same as the encoder, so this completes the circuit
+    # Decoder is the same as the encoder, so this completes the circuit
+    qc.append(cmulti_z_gate2, [4, 3, 2])
+    qc.cx(1, 4)
+    qc.cx(3, 2)
+    qc.append(cmulti_x_gate, [0, 2, 4])
+    qc.cx(2, 4)
+    qc.append(cmulti_z_gate1, [1, 2, 3, 4])
+    qc.append(cmulti_z_gate0, [1, 2, 3, 4])
+    qc.h(3)
+    qc.h(1)
+    qc.h(0)
 
-    qc.measure(q[2], c[0])
-
-    # SIMULATION
     qi_job = execute(qc, backend=qi_backend, shots=1)
     qi_result = qi_job.result()
-    histogram = qi_result.get_counts(qc)
+    probabilities_histogram = qi_result.get_probabilities(qc)
 
-    final_state = 2
-    for state, counts in histogram.items():
-        print("The initial state was 0, and after the process it is ", state[0])
-        final_state = int(state[0])
+    correct_prob = 0.0
+    wrong_prob = 0.0
+    for state, val in probabilities_histogram.items():
+        if int(state[2]) == 0:
+            correct_prob += val
+        else:
+            wrong_prob += val
 
-    assert final_state != 2
-    return final_state
+    print("The initial state was 0, and after the process it is 0 with probability", round(correct_prob, 3))
+    print("The initial state was 0, and after the process it is 1 with probability", round(wrong_prob, 3))
+
+    return correct_prob
 
 
 def logical_qubit_error(p_error, repetitions, code):
-    correct = 0
-    wrong = 0
+
     noise_model = uniform_uncorrelated_noise(p_error)
 
+    total_prob = 0
     for i in range(repetitions):
         print("run", i)
-        if code(noise_model) == 0:
-            correct += 1
-        else:
-            wrong += 1
+        correct_prob = code(noise_model)
+        total_prob += correct_prob
 
-    print("correct: ", correct)
-    print("wrong: ", wrong)
+    error_rate = 1 - (total_prob / repetitions)
 
-    print("The error rate is now", wrong / repetitions, "instead of the underlying", p_error)
-
-    return wrong / repetitions
+    print("The error rate is now", round(error_rate, 5), "instead of the underlying", p_error)
+    return error_rate
 
 
 if __name__ == '__main__':
