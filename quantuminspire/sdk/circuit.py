73beb392e54da23fa3c1c4da598ce0ffe0e8bd5a
Olaf Schusler <o.m.schusler@gmail.com>
2022-08-24 14:36:18 +0200

    [QI2-141] Implement circuit class (#4)

---
+++ quantuminspire/sdk/circuit.py
@@ -0,0 +1,109 @@
+"""Module containing the Quantum Circuit class."""
+
+from pathlib import Path
+from types import TracebackType
+from typing import Optional, Type
+
+import openql
+from openql import Kernel, Platform, Program
+
+
+class Circuit:
+    """A container object, interacting with OpenQL and storing cQASM
+    internally.
+
+    A circuit wraps OpenQL to handle the boilerplate code for platform,
+    program and kernels. These objects can still be used.
+    """
+
+    def __init__(self, platform_name: str, program_name: str) -> None:
+        self._output_dir = Path(__file__).parent.absolute() / "output"
+        openql.set_option("output_dir", str(self._output_dir))
+        self._platform_name = platform_name
+        self._program_name = program_name
+        self._openql_platform = Platform(self._platform_name, "none")
+        self._openql_program: Optional[Program] = None
+        self._openql_kernels: list[Kernel] = []
+        self._cqasm: str = ""
+
+    @property
+    def qasm(self) -> str:
+        """Return the quantum circuit.
+
+        Returns:
+            The string representation of the quantum circuit.
+        """
+        return self._cqasm
+
+    def __enter__(self) -> "Circuit":
+        self.initialize()
+        return self
+
+    def __exit__(
+        self, exc_type: Optional[Type[BaseException]], exc_val: Optional[BaseException], exc_tb: Optional[TracebackType]
+    ) -> bool:
+        self.finalize()
+        return True
+
+    def initialize(self) -> None:
+        """Initialize the quantum circuit."""
+
+    def finalize(self) -> None:
+        """Finalize the quantum circuit.
+
+        After finishing writing the quantum circuit various actions are
+        performed to generate the actual cQASM circuit. First, the used
+        number of qubits is determined, based on the various kernels. It
+        is assumed that the qubits will be reused over the various
+        kernels. This creates an OpenQL program, to which the various
+        kernels are added. Finally, the program is compiled and the
+        generated cQASM file is processed to an internal variable.
+        """
+        self._openql_program = openql.Program(self._program_name, self._openql_platform, self.max_number_of_qubits)
+        for kernel in self._openql_kernels:
+            self._openql_program.add_kernel(kernel)
+        self._openql_program.compile()
+        self._cqasm = self._process_cqasm_file()
+
+    @property
+    def max_number_of_qubits(self) -> int:
+        """Determine the number of qubits over the various kernels.
+
+        Returns:
+            The maximum number of qubits used in the kernels, assuming that the qubits can be reused.
+        """
+        return int(max((kernel.qubit_count for kernel in self._openql_kernels), default=0))
+
+    def _process_cqasm_file(self) -> str:
+        """Read and remove the generated cQASM file.
+
+        Returns:
+            The content of the OpenQL generated cQASM file.
+        """
+        cqasm_file = self._output_dir / f"{self._program_name}.qasm"
+        with open(cqasm_file, encoding="utf-8") as file_pointer:
+            cqasm = file_pointer.read()
+        Path.unlink(cqasm_file)
+        return cqasm
+
+    def init_kernel(self, name: str, number_of_qubits: int) -> Kernel:
+        """Initialize an OpenQL kernel.
+
+        A new OpenQL kernel is created and added to an internal list
+        (ordered) of kernels. This list will be used to
+        compile the final program (in order).
+
+        Args:
+            name: Name of the kernel.
+            number_of_qubits: Number of qubits used in the kernel.
+
+        Returns:
+            The OpenQL kernel.
+        """
+        kernel = Kernel(name, self._openql_platform, number_of_qubits)
+        self._openql_kernels.append(kernel)
+        return kernel
+
+    def add_kernel(self, kernel: Kernel) -> None:
+        """Add an existing kernel to the list of kernels."""
+        self._openql_kernels.append(kernel)
---
+++ tests/sdk/test_circuit.py
@@ -0,0 +1,61 @@
+from typing import Generator
+from unittest.mock import MagicMock
+
+import pytest
+from pytest_mock import MockerFixture
+
+from quantuminspire.sdk.circuit import Circuit
+
+MOCK_QUANTUM_CIRCUIT = "quantum circuit"
+
+
+@pytest.fixture
+def openql(mocker: MockerFixture) -> Generator[MagicMock, None, None]:
+    yield mocker.patch("quantuminspire.sdk.circuit.openql")
+
+
+@pytest.fixture
+def mock_file(mocker: MockerFixture) -> None:
+    mocker.patch("quantuminspire.sdk.circuit.open", mocker.mock_open(read_data=MOCK_QUANTUM_CIRCUIT))
+    mocker.patch("quantuminspire.sdk.circuit.Path.unlink")
+
+
+def test_create(openql: MagicMock) -> None:
+    _ = Circuit(platform_name="platform", program_name="program")
+    openql.set_option.assert_called_once()
+
+
+def test_create_empty_circuit(openql: MagicMock, mock_file: None) -> None:
+    with Circuit(platform_name="platform", program_name="program") as c:
+        pass
+
+    openql.Program().compile.assert_called_once()
+    assert c.qasm == MOCK_QUANTUM_CIRCUIT
+
+
+def test_create_circuit_with_kernel(openql: MagicMock, mock_file: None) -> None:
+    with Circuit(platform_name="platform", program_name="program") as c:
+        k = c.init_kernel("kernel1", 2)
+        k.x(0)
+
+    openql.Program().add_kernel.assert_called_once()
+    assert c.max_number_of_qubits == 2
+
+
+def test_create_circuit_with_multiple_kernels(openql: MagicMock, mock_file: None) -> None:
+    with Circuit(platform_name="platform", program_name="program") as c:
+        k = c.init_kernel("kernel1", 2)
+        k.x(0)
+        _ = c.init_kernel("kernel2", 3)
+
+    assert len(openql.Program().add_kernel.mock_calls) == 2
+    assert c.max_number_of_qubits == 3
+
+
+def test_create_circuit_reuse_kernel(openql: MagicMock, mock_file: None) -> None:
+    with Circuit(platform_name="platform", program_name="program") as c:
+        k = c.init_kernel("kernel1", 2)
+        k.x(0)
+        c.add_kernel(k)
+
+    assert len(openql.Program().add_kernel.mock_calls) == 2
