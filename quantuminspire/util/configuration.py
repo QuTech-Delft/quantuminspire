cf031a7de5c5967276cc43d1f6ef7f04bdd0a6f9
Harm Munk <harm.munk@tno.nl>
2023-03-14 17:48:06 +0100

    Added persistent configuration

---
+++ quantuminspire/util/configuration.py
@@ -0,0 +1,69 @@
+"""Module containing the handler for the Quantum Inspire persistent configuration."""
+
+import json
+from pathlib import Path
+from typing import Any, Optional
+
+from pydantic import BaseSettings
+
+
+def ensure_config_file_exists(file_path: Path, file_encoding: Optional[str]) -> None:
+    """Create the file if it does not exist.
+
+    Args:
+        file_path: the file path.
+        file_encoding: The encoding of the file.
+    """
+    if not file_path.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.open("w", encoding=file_encoding).write("{}")
+    return None
+
+
+def json_config_settings(settings: BaseSettings) -> Any:
+    """Settings source that loads variables from a JSON file specified in the Config class.
+
+    Args:
+        settings: The settings class, used to determine the file encoding, based on the encoding of the
+                  env_file_encoding
+    Returns:
+        A dictionary with the setting variables from the JSON file.
+    """
+    encoding: Optional[str] = settings.Config.env_file_encoding
+    json_config_file: Path = settings.Config.json_config_file  # type: ignore
+
+    ensure_config_file_exists(json_config_file, encoding)
+
+    # The next stmt will fail if the json_config_file is something else then a regular file, and, of course, if the
+    # file is not a valid JSON file.
+    return json.loads(json_config_file.read_text(encoding))
+
+
+class Settings(BaseSettings):
+    auths: str = "auths"
+
+    class Config:
+        env_file_encoding = "utf-8"
+        env_prefix = "QI2_"
+        json_config_file = Path.joinpath(Path.home(), ".quantuminspire", "config.json")
+
+        @classmethod
+        def customise_sources(cls, init_settings: Any, env_settings: Any, file_secret_settings: Any) -> Any:
+            """Customise the settings sources (by adding, removing and changing the order of sources).
+
+            Args:
+                init_settings: The initial settings (Settings object creation): highest priority.
+                env_settings: The configuration settings (Config inner object creation).
+                file_secret_settings: The file secret settings: lowest priority
+
+            Returns:
+                The original sources, with
+                - the JSON file as source added after the initial settings and before the configuration settings.
+                The order determines the priority!
+            """
+            return (
+                init_settings,
+                json_config_settings,
+                env_settings,
+                file_secret_settings,
+            )
---
+++ tests/util/test_configuration.py
@@ -0,0 +1,57 @@
+from unittest.mock import MagicMock
+
+import quantuminspire.util.configuration as configuration
+
+
+class TestCreate:
+    def test_force_file_into_existence_file_does_not_exist(self) -> None:
+        path = MagicMock()
+        path.exists.return_value = False
+        open_mock = MagicMock()
+        path.open.return_value = open_mock
+        configuration.ensure_config_file_exists(path, "utf-8")
+        path.parent.mkdir.assert_called_once_with(parents=True, exist_ok=True)
+        path.open.assert_called_once_with("w", encoding="utf-8")
+        open_mock.write.assert_called_once_with("{}")
+
+    def test_force_file_into_existence_file_exists(self) -> None:
+        path = MagicMock()
+        path.exists.return_value = True
+        open_mock = MagicMock()
+        path.open.return_value = open_mock
+        configuration.ensure_config_file_exists(path, "utf-8")
+        path.parent.mkdir.assert_not_called()
+        path.open.assert_not_called()
+        path.open().write.assert_not_called()
+        open_mock.write.assert_not_called()
+
+    def test_json_config_settings_file_does_not_exist(self) -> None:
+        settings = MagicMock()
+        settings.Config.env_file_encoding = "utf-8"
+        settings.Config.json_config_file = MagicMock()
+        settings.Config.json_config_file.exists.return_value = False
+        settings.Config.json_config_file.read_text.return_value = "{}"
+        assert configuration.json_config_settings(settings) == {}
+
+    def test_json_config_settings_file_does_exist(self) -> None:
+        settings = MagicMock()
+        settings.Config.env_file_encoding = "utf-8"
+        settings.Config.json_config_file = MagicMock()
+        settings.Config.json_config_file.exists.return_value = True
+        settings.Config.json_config_file.read_text.return_value = '{"auths": "authorisations from file"}'
+        assert configuration.json_config_settings(settings) == {"auths": "authorisations from file"}
+
+    def test_customise_sources(self) -> None:
+        init_settings = MagicMock()
+        env_settings = MagicMock()
+        file_secret_settings = MagicMock()
+        assert configuration.Settings.Config.customise_sources(init_settings, env_settings, file_secret_settings) == (
+            init_settings,
+            configuration.json_config_settings,
+            env_settings,
+            file_secret_settings,
+        )
+
+    def test_settings_from_init(self) -> None:
+        settings = configuration.Settings(auths="authorisations")
+        assert settings.auths == "authorisations"
