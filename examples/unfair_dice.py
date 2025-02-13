46d117890cd9a8b34bfe22f5c69d7cedf012d5dd
Lars van Leeuwen <larsvanleeuwen94@gmail.com>
2025-02-13 11:57:11 +0100

    [QI2-1441] Fix unfair dice example (#182)
    * Fixed out distribution
    
    * Fix example

--- examples/unfair_dice.py
+++ examples/unfair_dice.py
@@ -26,7 +26,7 @@ import numpy as np
 from opensquirrel.circuit_builder import CircuitBuilder
 from opensquirrel.ir import Bit, Float, Qubit
 from opensquirrel.writer import writer
-from qi2_shared.hybrid.quantum_interface import ExecuteCircuitResult, QuantumInterface
+from qi2_shared.hybrid.quantum_interface import QuantumInterface
 from qiskit_algorithms.optimizers import SPSA
 
 
@@ -102,7 +102,8 @@ m = 2**number_of_qubits
 p0 = np.random.random(m) + 0.2
 p0 = p0 / np.sum(p0)
 target_distribution = {k: p0[k] for k in range(m)}
-dt = AverageDecreaseTermination(N=35)
+dt = AverageDecreaseTermination(N=60)
+out_distribution = None
 
 
 def U(builder: CircuitBuilder, q: Qubit, theta: float, phi: float, lamb: float):
@@ -149,6 +150,8 @@ def objective_function(params: List[Any], qi: QuantumInterface, target_distribut
 
 
 def execute(qi: QuantumInterface) -> None:
+    global out_distribution
+
     optimizer = SPSA(maxiter=100, callback=None, termination_checker=dt)
 
     F = partial(objective_function, qi=qi, target_distribution=target_distribution, nshots=2000)
@@ -159,24 +162,12 @@ def execute(qi: QuantumInterface) -> None:
     )
     result = optimizer.minimize(fun=F, x0=initial_parameters)
 
-    return result
+    # Run the circuit again with optimized parameters
+    cqasm = generate_ansatz(result.x)
+    out_distribution = counts_to_distr(qi.execute_circuit(cqasm, 2000).results)
 
 
-def finalize(list_of_measurements: List[ExecuteCircuitResult]) -> Dict[str, Any]:
-    total_counts = {}
-    for measurement_results in list_of_measurements:
-        results = measurement_results
-        if (
-            "results" in measurement_results
-        ):  # local and remote return different objects (will fix in next version of sdk!)
-            results = measurement_results["results"]
+def finalize(list_of_measurements: List[Dict[str, Dict[str, int]]]) -> Dict[str, Any]:
+    global out_distribution
 
-        counts = counts_to_distr(results)
-        for k in counts:
-            if k in total_counts:
-                total_counts[k] += counts[k]
-            else:
-                total_counts[k] = counts[k]
-
-    total_counts = {k: total_counts[k] / len(list_of_measurements) for k in total_counts}
-    return {"avg_decrease": dt.values, "target_distribution": target_distribution, "out_distribution": total_counts}
+    return {"avg_decrease": dt.values, "target_distribution": target_distribution, "out_distribution": out_distribution}
