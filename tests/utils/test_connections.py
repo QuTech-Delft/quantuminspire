019beea3a3af139afeb114515bf2ae6e31f1cf3f
Olaf Schusler <o.m.schusler@tudelft.nl>
2025-11-03 16:06:44 +0100

    [QI2-1720] Setup CLI scaffolding (#342)

--- tests/utils/test_connections.py
+++ tests/utils/test_connections.py
@@ -31,3 +31,12 @@ def test_add_protocol_raises_exception(mocker: MockerFixture) -> None:
 
     url = connections.add_protocol("host")
     assert url == "http://host"
+
+
+def test_url_passed_to_requests_head(mocker: MockerFixture) -> None:
+    mock_head = mocker.patch("quantuminspire.utils.connections.requests.head")
+    mock_head.return_value.status_code = 200
+
+    connections.add_protocol("host")
+
+    mock_head.assert_called_once_with("https://host", timeout=3, allow_redirects=True)
