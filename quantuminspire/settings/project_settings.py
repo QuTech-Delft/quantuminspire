fb5387a582d3d4ef0788a90ac0b0b75b6a8e7356
Nischal Sehrawat <146720509+NischalQuTech@users.noreply.github.com>
2025-12-19 09:22:09 +0100

    [QI2-1722] Implemented config manager (#355)
    * [QI2-1722] Implemented config manager
    
    * use macos-intel
    
    * review 1
    
    * Updated workflows file
    
    * Removed env variable definition from config

---
+++ quantuminspire/settings/project_settings.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from typing import Any, Dict
+
+from quantuminspire.settings.base_settings import BaseConfigSettings, ensure_config_file_exists
+from quantuminspire.settings.models import Algorithm, Job, Project
+
+
+class ProjectSettings(BaseConfigSettings):
+
+    algorithm: Algorithm = Algorithm()
+    project: Project = Project()
+    job: Job = Job()
+
+    @classmethod
+    def find_project_root(cls, start: Path | None = None) -> Path:
+        """Search upward from start until the project marker is found."""
+        start = start or Path.cwd()
+
+        current = start.resolve()
+
+        while True:
+            marker = current.joinpath(*cls.file_marker())
+            if marker.exists():
+                return current
+
+            if current.parent == current:  # Reached filesystem root
+                break
+
+            current = current.parent
+
+        raise FileNotFoundError("Project root not found")
+
+    @classmethod
+    def base_dir(cls) -> Path:
+        return cls.find_project_root()
+
+    @classmethod
+    def default_factory(cls) -> Dict[str, Any]:
+        return {}
+
+    @classmethod
+    def initialize(cls, path: Path) -> None:
+        ensure_config_file_exists(
+            file_path=cls.json_file(path=path),
+            file_encoding=cls.model_config.get("env_file_encoding"),
+            default_factory=cls.default_factory,
+        )
