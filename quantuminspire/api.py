883fe5d8bb1ba6e1883ba29e4afe861fe7b4c4c6
nienkeduetz <nienke.duetz@navara.nl>
2026-02-18 12:33:38 +0000

    algorithm execution manager

--- quantuminspire/api.py
+++ quantuminspire/api.py
@@ -1,19 +1,25 @@
+import time
+from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, cast
 from urllib.parse import urlparse
 
 import requests
 from compute_api_client import (
+    Algorithm,
+    AlgorithmType,
     BackendType,
     FinalResult,
     Job,
+    JobStatus,
+    Project,
 )
 from pydantic import TypeAdapter
 
 from quantuminspire.managers.auth_manager import AuthManager
 from quantuminspire.managers.config_manager import ConfigManager
 from quantuminspire.managers.job_manager import JobManager, JobOptions
-from quantuminspire.settings.models import Url
+from quantuminspire.settings.models import LocalAlgorithm, Url
 
 
 class Api:
@@ -26,7 +32,7 @@ class Api:
     ) -> None:
         self._config_manager = config_manager or ConfigManager()
         self._auth_manager = auth_manager or AuthManager(user_settings=self._config_manager.user_settings)
-        self._job_manager = job_manager or JobManager(self._auth_manager)
+        self._job_manager = job_manager or JobManager(self._config_manager)
 
     def login(self, hostname: Optional[str] = None, override_auth_config: bool = False, force: bool = False) -> None:
         host_url: Optional[Url] = None
@@ -48,22 +54,43 @@ class Api:
     def submit_job(
         self,
         file_path: str,
-        file_name: Optional[str] = None,
+        algorithm_name: Optional[str],
         num_shots: Optional[int] = None,
         store_raw_data: Optional[bool] = None,
         backend_type_id: Optional[int] = None,
-        persist: bool = True,
+        persist: bool = False,
     ) -> JobOptions:
 
-        options: Dict[str, Any] = self._get_job_options(file_name, num_shots, store_raw_data, backend_type_id)
+        if not persist and algorithm_name is None:
+            if backend_type_id is None:
+                raise ValueError("backend_type_id not provided")
+
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            options: Dict[str, Any] = {
+                "project_name": f"Project created on {timestamp}",
+                "project_description": "Project created for non-persistent algorithm",
+                "algorithm_name": f"Algorithm created on {timestamp}",
+                "backend_type_id": backend_type_id,
+                "number_of_shots": num_shots,
+                "raw_data_enabled": store_raw_data,
+            }
+        elif persist and algorithm_name is None:
+            raise ValueError("algorithm_name must be provided when persist is True")
+        else:
+            options = self._get_job_options(cast(str, algorithm_name), num_shots, store_raw_data, backend_type_id)
 
         job_options = self._job_manager.run_flow(JobOptions(**(options | {"file_path": Path(file_path)})))
 
-        self._config_manager.set("project.id", job_options.project_id, is_user=False)
-
         if persist:
-            self._config_manager.set("job.id", job_options.job_id, is_user=False)
-            self._config_manager.set("algorithm.name", job_options.algorithm_name, is_user=False)
+            local_algorithm = LocalAlgorithm(
+                id=job_options.algorithm_id,
+                file_path=str(job_options.file_path),
+                num_shots=job_options.number_of_shots,
+                store_raw_data=job_options.raw_data_enabled,
+                job_id=job_options.job_id,
+                backend_type_id=backend_type_id,
+            )
+            self.add_algorithm_to_settings(job_options.algorithm_name, local_algorithm)
 
         return job_options
 
@@ -80,10 +107,50 @@ class Api:
         assert job_id is not None
         return self._job_manager.get_final_result(job_id)
 
+    def wait_for_job_completion(self, job_id: Optional[int] = None, timeout: Optional[int] = 60) -> Job:
+        start_time = time.time()
+
+        print("Waiting for job to complete...")
+        while True:
+            job = self.get_job(job_id)
+
+            if job.status not in [JobStatus.PLANNED, JobStatus.RUNNING]:
+                return job
+
+            if timeout is not None and time.time() - start_time >= timeout:
+                raise TimeoutError(f"Job {job.id} did not complete within {timeout} seconds")
+
+            time.sleep(1)
+
+    def _get_local_algorithm(self, algorithm_name: str) -> LocalAlgorithm:
+        algorithms = self._config_manager.get("project.algorithms")
+        try:
+            local_algorithm: LocalAlgorithm = algorithms[algorithm_name]
+            return local_algorithm
+        except KeyError:
+            raise ValueError(f"Algorithm '{algorithm_name}' not found in settings.")
+
+    def add_algorithm_to_settings(self, algorithm_name: str, local_algorithm: LocalAlgorithm) -> None:
+        algorithms = self._config_manager.get("project.algorithms")
+        algorithms[algorithm_name] = local_algorithm
+        self._config_manager.set("project.algorithms", algorithms, is_user=False)
+
     @staticmethod
     def initialize_project(path: Optional[Path] = None) -> None:
         init_dir = path or Path.cwd()
         ConfigManager.initialize(init_dir)
+
+    def initialize_remote_project(self, project_name: str, project_description: Optional[str] = "") -> Project:
+        host = self._config_manager.get("default_host")
+        owner_id = self._config_manager.user_settings.auths[host].owner_id
+        return self._job_manager.create_project(owner_id, project_name, cast(str, project_description))
+
+    def create_remote_algorithm(self, project_id: int, algorithm_name: str, file_path: str) -> Algorithm:
+        if file_path:
+            algorithm_type = self._job_manager.get_algorithm_type(Path(file_path))
+        else:
+            algorithm_type = AlgorithmType.HYBRID
+        return self._job_manager.create_algorithm(project_id, algorithm_name, algorithm_type)
 
     def view_settings(self) -> Dict[str, Any]:
         return self._config_manager.inspect()
@@ -91,12 +158,21 @@ class Api:
     def get_setting(self, key: str) -> Any:
         return self._config_manager.get(key)
 
+    def get_algorithm_setting(self, algorithm_name: str, setting_name: str) -> Any:
+        local_algorithm = self._get_local_algorithm(algorithm_name)
+        return getattr(local_algorithm, setting_name)
+
     def set_setting(self, key: str, value: Any, is_user: bool = False) -> None:
         return self._config_manager.set(key, value, is_user)
 
+    def set_algorithm_setting(self, algorithm_name: str, setting_name: str, value: Any) -> None:
+        local_algorithm = self._get_local_algorithm(algorithm_name)
+        setattr(local_algorithm, setting_name, value)
+        self.add_algorithm_to_settings(algorithm_name, local_algorithm)
+
     def _get_job_options(
         self,
-        file_name: Optional[str] = None,
+        algorithm_name: str,
         num_shots: Optional[int] = None,
         store_raw_data: Optional[bool] = None,
         backend_type_id: Optional[int] = None,
@@ -106,13 +182,18 @@ class Api:
             "project_id": self._config_manager.get("project.id"),
             "project_name": self._config_manager.get("project.name"),
             "project_description": self._config_manager.get("project.description"),
-            "backend_type": self._resolve(backend_type_id, "backend_type"),
-            "number_of_shots": self._resolve(num_shots, "algorithm.num_shots"),
-            "raw_data_enabled": self._resolve(store_raw_data, "algorithm.store_raw_data"),
-            "algorithm_name": self._resolve(file_name, "algorithm.name"),
+            "algorithm_id": self._resolve_algorithm_setting(None, "id", algorithm_name),
+            "backend_type_id": self._resolve_algorithm_setting(backend_type_id, "backend_type_id", algorithm_name),
+            "number_of_shots": self._resolve_algorithm_setting(num_shots, "num_shots", algorithm_name),
+            "raw_data_enabled": self._resolve_algorithm_setting(store_raw_data, "store_raw_data", algorithm_name),
+            "algorithm_name": algorithm_name,
         }
 
         return options
+
+    def _resolve_algorithm_setting(self, override: Optional[Any], setting_name: str, algorithm_name: str) -> Any:
+        local_algorithm = self._get_local_algorithm(algorithm_name)
+        return override if override is not None else getattr(local_algorithm, setting_name)
 
     def _resolve(self, override: Optional[Any], key: str) -> Any:
         return override if override is not None else self._config_manager.get(key)
